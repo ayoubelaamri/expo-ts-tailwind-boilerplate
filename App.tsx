cb6d3202cb03a20d9e2db2dd8d614bde95ed7a8d
SenorJob <senorjob39@gmail.com>
2023-10-21 22:21:31 +0100

    add Tailwind

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { NativeWindStyleSheet } from "nativewind";
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   const title:string = 'myApp'
 
   return (
-    <View style={styles.container}>
-      <Text>{title}</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 text-3xl font-bold">{title}</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
